8a9aed4280a4a6dcb5564e12b0ab9cfd51364a4a
Stefan Seegerer <stefan.seegerer@meetiqm.com>
2025-10-09 22:04:38 +0200

    add 01-a5

---
+++ 01/01-a5/script.js
@@ -0,0 +1,9 @@
+// - Klick auf #add-task soll eine neue Aufgabe hinzufügen
+// - Aufgaben sollen als Bootstrap Cards angezeigt werden
+// - Jede Aufgabe soll einen Delete-Button enthalten
+// - Delete-Button soll per jQuery die Card entfernen (mit .fadeOut())
+// - Badge (#task-count) soll immer die Anzahl offener Aufgaben anzeigen
+
+$(document).ready(function() {
+  // TODO: Aufgabe hier umsetzen
+});
